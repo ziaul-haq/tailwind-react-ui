3684c8506f1fc23ce188fb6d089b33f0b404680b
Iftekhar Rifat <iftekhar.alam@bkash.com>
2021-01-26 19:51:13 +0600

    Add dark mode control in storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,14 @@
 import './tailwind.css'
+import { themes } from '@storybook/theming'
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
+  darkMode: {
+    dark: themes.dark,
+    light: themes.normal,
+    darkClass: 'dark',
+    lightClass: 'light',
+    stylePreview: true,
+  },
   layout: 'centered',
 }
