53a7249380ccd71e9fc884b7c682080f891d26e2
Md. Sazzadul Islam <sazzadul.islam@bkash.com>
2021-06-27 12:51:02 +0600

    fix: button left and right icon storybook

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -45,9 +45,15 @@ export default {
 
 const Template: Story<ButtonProps> = (args) => (
   <section>
-    <Button className="mr-4" layout="primary" {...args}>Primary</Button>
-    <Button className="mr-4" layout="outline" {...args}>Outline</Button>
-    <Button className="mr-4" layout="link" {...args}>Link</Button>
+    <Button className="mr-4" layout="primary" {...args}>
+      Primary
+    </Button>
+    <Button className="mr-4" layout="outline" {...args}>
+      Outline
+    </Button>
+    <Button className="mr-4" layout="link" {...args}>
+      Link
+    </Button>
   </section>
 )
 
@@ -59,15 +65,29 @@ const IconOnlyTemplate: Story<ButtonProps> = (args) => (
   </section>
 )
 
-export const EditIcon = ({ className = '' }: { className?: string}) => (
-  <svg className={className} aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
+export const EditIcon = ({ className = '' }: { className?: string }) => (
+  <svg
+    className={className}
+    aria-hidden="true"
+    fill="currentColor"
+    viewBox="0 0 20 20"
+  >
     <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"></path>
   </svg>
 )
 
-export const HeartIcon = ({ className }: { className?: string}) => (
-  <svg className={className} aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
-    <path d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clipRule="evenodd" fillRule="evenodd"></path>
+export const HeartIcon = ({ className }: { className?: string }) => (
+  <svg
+    className={className}
+    aria-hidden="true"
+    fill="currentColor"
+    viewBox="0 0 20 20"
+  >
+    <path
+      d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
+      clipRule="evenodd"
+      fillRule="evenodd"
+    ></path>
   </svg>
 )
 
@@ -81,10 +101,10 @@ IconOnly.args = {
 
 export const LeftIcon = Template.bind({})
 LeftIcon.args = {
-  iconLeft: () => <HeartIcon className="mr-2 -ml-1" />,
+  iconLeft: () => <HeartIcon className="mr-2 -ml-1 w-4 h-4" />,
 }
 
 export const RightIcon = Template.bind({})
 RightIcon.args = {
-  iconRight: () => <EditIcon className="ml-2 -mr-1" />,
+  iconRight: () => <EditIcon className="ml-2 -mr-1 w-4 h-4" />,
 }
